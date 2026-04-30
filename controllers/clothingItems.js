42987a68f9c1e46051ef699f6e40eca0dc3b8a14
Jarryn Hallare <jarrynhallare@gmail.com>
2026-04-30 09:32:48 -0500

    refresh

--- controllers/clothingItems.js
+++ controllers/clothingItems.js
@@ -8,7 +8,7 @@ const createItem = (req, res) => {
     return res.status(400).send({ message: "Missing required fields: name, weather, imageURL" });
   }
 
-  return ClothingItem.create({ name, weather, imageURL })
+  ClothingItem.create({ name, weather, imageURL })
     .then((item) => res.status(201).send({ data: item }))
     .catch((e) => {
       console.error(e);
@@ -20,8 +20,8 @@ const createItem = (req, res) => {
 };
 
 const getItems = (req, res) => {
-  return ClothingItem.find({})
-    .then((items) => res.status(200).send(items))
+  ClothingItem.find({})
+    .then((items) => res.status(200).send({ data: items }))
     .catch((e) => {
       console.error(e);
       return res.status(DEFAULT_ERROR).send({ message: "An error has occurred on the server" });
@@ -36,13 +36,20 @@ const updateItem = (req, res) => {
     return res.status(400).send({ message: "imageURL is required" });
   }
 
-   return ClothingItem.findByIdAndUpdate(itemId, { $set: { imageURL } }, { new: true })
+  ClothingItem.findByIdAndUpdate(
+    itemId,
+    { $set: { imageURL } },
+    { new: true, runValidators: true }
+  )
     .orFail()
     .then((item) => res.status(200).send({ data: item }))
     .catch((e) => {
       console.error(e);
       if (e.name === "DocumentNotFoundError") {
         return res.status(404).send({ message: "Item not found" });
+      }
+      if (e.name === "CastError") {
+        return res.status(400).send({ message: "Invalid item ID" });
       }
       return res.status(DEFAULT_ERROR).send({ message: "An error has occurred on the server" });
     });
@@ -51,13 +58,16 @@ const updateItem = (req, res) => {
 const deleteItem = (req, res) => {
   const { itemId } = req.params;
 
-  return ClothingItem.findByIdAndDelete(itemId)
+  ClothingItem.findByIdAndDelete(itemId)
     .orFail()
     .then(() => res.status(204).send({}))
     .catch((e) => {
       console.error(e);
       if (e.name === "DocumentNotFoundError") {
         return res.status(404).send({ message: "Item not found" });
+      }
+      if (e.name === "CastError") {
+        return res.status(400).send({ message: "Invalid item ID" });
       }
       return res.status(DEFAULT_ERROR).send({ message: "An error has occurred on the server" });
     });
@@ -68,5 +78,4 @@ module.exports = {
   getItems,
   updateItem,
   deleteItem,
-
-    };
+};
