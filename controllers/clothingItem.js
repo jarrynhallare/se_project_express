5bab1ffb1cd588a91490b25ce3ebf9d37c7f0e3b
Jarryn Hallare <jarrynhallare@gmail.com>
2026-05-04 20:38:24 -0500

    updated

--- controllers/clothingItem.js
+++ controllers/clothingItem.js
@@ -11,20 +11,27 @@ const createItem = (req, res) => {
     imageUrl,
     owner: req.user._id,
   })
-    .then((item) => res.status(201).send(item))
+    .then((item) => {
+      res.status(201).send(item);
+    })
     .catch((err) => {
       if (err.name === "ValidationError") {
-        return res.status(400).send({ message: "Invalid data" });
+        res.status(400).send({ message: "Invalid data" });
+      } else {
+        res.status(500).send({ message: "Server error" });
       }
-      return res.status(500).send({ message: "Server error" });
     });
 };
 
 // GET ITEMS
 const getItems = (req, res) => {
   ClothingItem.find({})
-    .then((items) => res.status(200).send(items))
-    .catch(() => res.status(500).send({ message: "Server error" }));
+    .then((items) => {
+      res.status(200).send(items);
+    })
+    .catch(() => {
+      res.status(500).send({ message: "Server error" });
+    });
 };
 
 // DELETE ITEM
@@ -32,44 +39,54 @@ const deleteItem = (req, res) => {
   const { itemId } = req.params;
 
   if (!mongoose.Types.ObjectId.isValid(itemId)) {
-    return res.status(400).send({ message: "Invalid item ID" });
+    res.status(400).send({ message: "Invalid item ID" });
+    return;
   }
 
   ClothingItem.findByIdAndDelete(itemId)
     .orFail()
-    .then((item) => res.status(200).send(item))
+    .then((item) => {
+      res.status(200).send(item);
+    })
     .catch((err) => {
       if (err.name === "DocumentNotFoundError") {
-        return res.status(404).send({ message: "Not found" });
+        res.status(404).send({ message: "Not found" });
+      } else {
+        res.status(500).send({ message: "Server error" });
       }
-      return res.status(500).send({ message: "Server error" });
     });
 };
 
-// LIKE / UNLIKE LOGIC
+// LIKE / UNLIKE
 const updateLike = (req, res, method) => {
   const { itemId } = req.params;
 
   if (!req.user || !req.user._id) {
-    return res.status(401).send({ message: "Unauthorized" });
+    res.status(401).send({ message: "Unauthorized" });
+    return;
   }
 
   if (!mongoose.Types.ObjectId.isValid(itemId)) {
-    return res.status(400).send({ message: "Invalid item ID" });
+    res.status(400).send({ message: "Invalid item ID" });
+    return;
   }
 
-  const update = method === "like"
-    ? { $addToSet: { likes: req.user._id } }
-    : { $pull: { likes: req.user._id } };
+  const update =
+    method === "like"
+      ? { $addToSet: { likes: req.user._id } }
+      : { $pull: { likes: req.user._id } };
 
   ClothingItem.findByIdAndUpdate(itemId, update, { new: true })
     .orFail()
-    .then((item) => res.status(200).send(item))
+    .then((item) => {
+      res.status(200).send(item);
+    })
     .catch((err) => {
       if (err.name === "DocumentNotFoundError") {
-        return res.status(404).send({ message: "Not found" });
+        res.status(404).send({ message: "Not found" });
+      } else {
+        res.status(500).send({ message: "Server error" });
       }
-      return res.status(500).send({ message: "Server error" });
     });
 };
 
