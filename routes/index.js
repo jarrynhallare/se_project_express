3da83bb6b675321f79b78c68a00b69ec1af61ed9
Jarryn Hallare <jarrynhallare@gmail.com>
2026-06-07 09:39:15 -0500

    updated

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,15 @@
 const router = require("express").Router();
 const { NON_EXISTENT } = require("../utils/errors");
+const { login, createUser } = require("../controllers/users");
+const auth = require("../middlewares/auth");
 const clothingItemRouter = require("./clothingItem");
 const userRouter = require("./users");
 
+router.post("/signin", login);
+router.post("/signup", createUser);
+
 router.use("/items", clothingItemRouter);
-router.use("/users", userRouter);
+router.use("/users", auth, userRouter);
 
 router.use((req, res) => {
   res.status(NON_EXISTENT).send({ message: "Not found" });
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,7 @@
 const router = require("express").Router();
-const { getUsers, createUser, login, getUserById } = require("../controllers/users");
+const { getUsers, getUserById } = require("../controllers/users");
 
 router.get("/", getUsers);
-router.post("/", createUser);
-router.post("/login", login);
 router.get("/:userId", getUserById);
 
 module.exports = router;
