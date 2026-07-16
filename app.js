94e31ea896f086075fb3a20419ca42c311261c64
Jarryn Hallare <jarrynhallare@gmail.com>
2026-07-16 12:06:42 -0500

    update

--- app.js
+++ app.js
@@ -9,6 +9,13 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.use((req, res, next) => {
+  req.user = {
+    _id: "5d8b8592978f8bd833ca8133",
+  };
+  next();
+});
+
 mongoose
   .connect("mongodb://127.0.0.1:27017/wtwr_db")
   .then(() => console.log("Connected"))
@@ -16,9 +23,8 @@ mongoose
 
 app.use("/", mainRouter);
 
-
 app.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}`);
+  console.log(`Listening on port ${PORT}`);
 });
 
 module.exports = app;
