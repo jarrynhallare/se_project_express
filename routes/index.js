875e637b80f29e597f8609a49b1481e1c988e3d7
Jarryn Hallare <jarrynhallare@gmail.com>
2026-07-16 12:02:49 -0500

    update

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,17 @@
 const router = require("express").Router();
 const { NON_EXISTENT } = require("../utils/errors");
 const { login, createUser } = require("../controllers/users");
+const { getItems } = require("../controllers/clothingItem");
 const auth = require("../middlewares/auth");
 const clothingItemRouter = require("./clothingItem");
 const userRouter = require("./users");
 
 router.post("/signin", login);
 router.post("/signup", createUser);
+
+
+router.get("/items", getItems);
+
 
 router.use("/items", auth, clothingItemRouter);
 router.use("/users", auth, userRouter);
